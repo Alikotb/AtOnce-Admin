8cbfacbb3131414dcd8aac7e58094d09a146c639
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-06-23 16:45:10 +0300

    feat: Implement Language Dropdown Menu
    This commit introduces a new composable function, `LanguageDropdownMenu`, which allows users to select their preferred language from a dropdown menu.
    
    The dropdown menu displays options for English, Arabic, and System Default, each accompanied by its respective flag icon.
    
    The selected language is then communicated back via the `onLanguageSelected` callback.

--- app/src/main/java/com/example/atonce_admin/presentation/profile/view/components/ProfileTopBar.kt
+++ app/src/main/java/com/example/atonce_admin/presentation/profile/view/components/ProfileTopBar.kt
@@ -1,30 +1,19 @@
 package com.example.atonce_admin.presentation.profile.view.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Language
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.atonce_admin.R
 import com.example.atonce_admin.core.enums.LanguageEnum
 import com.example.atonce_admin.presentation.common.theme.MediumFont
 
